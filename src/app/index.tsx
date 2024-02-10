786a1aeb5f1b8d08025ba05e6fbca06c601962bc
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-02-09 22:07:39 -0300

    creating _layout for all screens

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 export default function Home() {
   return (
-    <View className="flex-1 bg-slate-900" >
+    <View className="flex-1 pt-8" >
       <Text className='text-white'>React Native</Text>
     </View>
   );
