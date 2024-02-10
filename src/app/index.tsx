e07aa311efeac3284ff91968bbd7d6675af2cdb8
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-02-09 21:22:35 -0300

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>
+        Bem vindo ao React Native
+      </Text>
+    </View>
+  );
+}
