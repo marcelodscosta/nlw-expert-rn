5b7eec6febcee553ac2ccc3bf11e8b19fb251c40
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-02-09 22:31:23 -0300

    installing fonts and loading, creating loading

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from 'react-native';
 
+
 export default function Home() {
   return (
     <View className="flex-1 pt-8" >
-      <Text className='text-white'>React Native</Text>
+      <Text className='text-white text-2xl'>React Native</Text>
     </View>
   );
 }
